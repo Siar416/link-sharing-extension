e54faf896f681b63b1c4fa317b95f653cd8bb732
Siar Wahidi <siar.wahidi@gmail.com>
2025-07-27 11:55:02 -0400

    added rate limiter, will need to be tweeked for when the user sign up route and functionality is completed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import smsRoutes from "./routes/sms.routes.js";
 
 dotenv.config();
 
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: `Sorry, you've reached your max number to links you can share: ${max}, please sign up to get 20 link shares an hour`,
+  headers: true,
+});
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api/sms", smsRoutes);
 
